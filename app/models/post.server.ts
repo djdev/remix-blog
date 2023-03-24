b45e05fb74b6223b7261fb33990d5cb42969f352
DJ <deepjyoti_rajkhowa@yahoo.com>
2023-03-23 20:01:32 -0400

    create posts with mock data

---
+++ app/models/post.server.ts
@@ -0,0 +1,17 @@
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export async function getPosts(): Promise<Array<Post>> {
+  return [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A mixtape I have made just for you",
+    },
+  ];
+}
---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,37 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A mixtape I have made just for you",
+      },
+    ],
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+
+  return (
+    <main>
+      <h1>Posts</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
